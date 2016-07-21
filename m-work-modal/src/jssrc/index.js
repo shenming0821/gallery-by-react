da66a03316147397fc36e8b9c4a835639fb2a322
shenming <ming.shen@51auto.com>
2016-07-21 14:19:53 +0800

    更新

---
+++ m-work-modal/src/jssrc/index.js
@@ -0,0 +1,9 @@
+require([
+    "http://cdn01.51autoimg.com/51auto/js/lib/jquery.min.js",
+    "http://192.168.2.181/apps/zhuanti.51auto.com/xuetielong.20160518/js/swiper-3.3.1.min.js"
+], function (jquery,swiper) {
+    
+    $(() => {
+        new ;
+    });
+});
